576f37f261b591b95e1247245ff578e38803f7d7
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-14 19:20:19 +0300

    feat: display content depending on whether the user is logged in or not

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
 import LoginPage from "./pages/LoginPage";
 import AppLayout from "./ui/AppLayout";
+import Span from "./ui/form/Span";
+import { UserCredentials } from "./features/auth/LoginForm";
 
 export default class App {
   appLayout: AppLayout;
 
   constructor() {
     this.appLayout = new AppLayout();
-    this.appLayout.setContent(new LoginPage());
 
     document.body.append(this.appLayout.getElement());
+
+    this.configure();
+  }
+
+  configure() {
+    const userCredentialsString = localStorage.getItem("userCredentials");
+
+    if (userCredentialsString) {
+      // NOTE: it looks like there is no way around typecasting with JSON.parse()
+      const credentials = JSON.parse(userCredentialsString) as UserCredentials;
+
+      // TODO: redirect to start page instead
+      this.appLayout.setContent(
+        new Span("", `Welcome, ${credentials.firstName}`),
+      );
+    } else {
+      this.appLayout.setContent(new LoginPage());
+    }
   }
 }
