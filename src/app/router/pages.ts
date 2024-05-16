5d9fe9a1247251223c9d9da85b80efb440711022
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-16 21:35:22 +0300

    refactor: replace magic path values with constant

--- src/app/router/pages.ts
+++ src/app/router/pages.ts
@@ -1,7 +1,7 @@
-export enum Pages {
-  LOGIN = "login",
-  START = "start",
-  NOT_FOUND = "not-found",
-}
+export const Pages = {
+  LOGIN: "login",
+  START: "start",
+  NOT_FOUND: "not-found",
+};
 
 export const RESOURCE_SELECTOR = "";
