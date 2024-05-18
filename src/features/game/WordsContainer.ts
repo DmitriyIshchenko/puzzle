9a94341d47067ace5300f5bff2056fa8b1756377
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-18 20:58:57 +0300

    feat: calculate cards width

--- src/features/game/WordsContainer.ts
+++ src/features/game/WordsContainer.ts
@@ -4,6 +4,8 @@ import { div } from "../../ui/tags";
 import styles from "./WordsContainer.module.css";
 
 export default class WordsContainer extends Component {
+  private words: Array<string> = [];
+
   constructor(private sentence: string) {
     super({
       tag: "div",
@@ -14,10 +16,26 @@ export default class WordsContainer extends Component {
   }
 
   private configure() {
-    const words = this.sentence
-      .split(" ")
-      .map((word) => div({ className: styles.word, text: word }));
+    this.words = this.sentence.split(" ").sort(() => Math.random() - 0.5);
 
-    this.appendChildren(words);
+    this.updateWords();
+  }
+
+  private updateWords() {
+    this.clear();
+
+    this.appendChildren(
+      this.words.map((word) => {
+        const card = div({ className: styles.word, text: word });
+        card.getElement().style.width = `${this.calculateCardWidth(word).toString()}%`;
+
+        return card;
+      }),
+    );
+  }
+
+  private calculateCardWidth(word: string) {
+    const totalCharacters = this.sentence.split(" ").join("").length;
+    return (word.length * 100) / totalCharacters;
   }
 }
