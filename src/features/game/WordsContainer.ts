f12569974abb37f4b9ffa38a08ccf7c958795521
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-18 21:31:10 +0300

    feat: move card to the result block

--- src/features/game/WordsContainer.ts
+++ src/features/game/WordsContainer.ts
@@ -3,6 +3,11 @@ import { div } from "../../ui/tags";
 
 import styles from "./WordsContainer.module.css";
 
+export type WordData = {
+  word: string;
+  width: number;
+};
+
 export default class WordsContainer extends Component {
   private words: Array<string> = [];
 
@@ -29,9 +34,31 @@ export default class WordsContainer extends Component {
         const card = div({ className: styles.word, text: word });
         card.getElement().style.width = `${this.calculateCardWidth(word).toString()}%`;
 
+        card.addListener("click", () => {
+          this.moveWord(word);
+        });
+
         return card;
       }),
     );
+  }
+
+  private moveWord(word: string) {
+    const index = this.words.indexOf(word);
+
+    this.words.splice(index, 1);
+
+    // TODO: use observer pattern
+    window.dispatchEvent(
+      new CustomEvent<WordData>("move-word", {
+        detail: {
+          word,
+          width: this.calculateCardWidth(word),
+        },
+      }),
+    );
+
+    this.updateWords();
   }
 
   private calculateCardWidth(word: string) {
