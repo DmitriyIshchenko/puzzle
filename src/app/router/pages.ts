674b9400d4574a53b155cb98bdbd724e33a6cb60
Dmitriy Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-18 15:17:09 +0300

    feat: PZ-09 Navigate to the main game page (#24)
    * feat: create a game page component
    
    * feat: create a route for game page
    
    * refactor: change not found page layout

--- src/app/router/pages.ts
+++ src/app/router/pages.ts
@@ -1,6 +1,11 @@
+/* This looks like a good opportunity to use an enum, 
+but ESLint with strict type checking enabled throws a fit when you try to compare a primitive string to an enum member
+*/
+
 export const Pages = {
   LOGIN: "login",
   START: "start",
+  GAME: "game",
   NOT_FOUND: "not-found",
 };
 
