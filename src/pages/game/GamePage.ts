0a8ebdae10a2f9327292958ef32d2670996c542c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-18 19:45:50 +0300

    feat: design game page basic layout

--- src/pages/game/GamePage.ts
+++ src/pages/game/GamePage.ts
@@ -1,4 +1,8 @@
 import Component from "../../shared/Component";
+import GameField from "../../features/game/GameField";
+import WordsContainer from "../../features/game/WordsContainer";
+
+import wordsData from "../../../data/words.json";
 
 import styles from "./GamePage.module.css";
 
@@ -6,8 +10,17 @@ export default class GamePage extends Component {
   constructor() {
     super({
       tag: "main",
-      text: "GAME PAGE",
       className: styles.page,
     });
+    this.configure();
+  }
+
+  private configure() {
+    const gameField = new GameField();
+
+    // TEMP
+    const words = new WordsContainer(wordsData.rounds[0].words[0].textExample);
+
+    this.appendChildren([gameField, words]);
   }
 }
