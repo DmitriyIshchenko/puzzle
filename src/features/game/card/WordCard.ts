aec813d83ee7ecb2cd90cbd84b21dcf263da48ce
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-31 15:56:53 +0300

    feat: highlight hovered cell

--- src/features/game/card/WordCard.ts
+++ src/features/game/card/WordCard.ts
@@ -6,7 +6,7 @@ import { assertNonNull } from "../../../shared/helpers";
 import styles from "./WordCard.module.css";
 import rowStyles from "../fields/Row.module.css";
 
-const DRAG_THRESHOLD = 5;
+const DRAG_THRESHOLD = 2;
 
 export default class WordCard extends Component {
   private clientX: number = 0;
@@ -16,6 +16,8 @@ export default class WordCard extends Component {
   private shiftX: number = 0;
 
   private shiftY: number = 0;
+
+  private lastDropTarget: HTMLElement | null = null;
 
   constructor(
     data: Word,
@@ -49,6 +51,7 @@ export default class WordCard extends Component {
     // snap the card from the row
     card.style.position = "absolute";
     card.style.zIndex = "1000";
+    card.style.cursor = "grabbing";
     document.body.append(card);
 
     // follow the cursor
@@ -70,9 +73,27 @@ export default class WordCard extends Component {
 
   private mouseMoveHandler(e: MouseEvent): void {
     this.moveAt(e.pageX, e.pageY);
+
+    const dropTarget = WordCard.findDroppableCell(e.clientX, e.clientY);
+
+    if (!(dropTarget instanceof HTMLElement)) return;
+
+    if (this.lastDropTarget !== dropTarget) {
+      // leaving (cell -> null or other cell )
+      if (this.lastDropTarget) {
+        this.resetDropTargetHoverStyles();
+      }
+      this.lastDropTarget = dropTarget;
+
+      // entering (null -> cell)
+      this.setDropTargetHoverStyles();
+    }
   }
 
   private mouseUpHandler(e: MouseEvent) {
+    // otherwise cell stays highlighted after clicking on previously dragged card
+    this.resetDropTargetHoverStyles();
+
     const isDrag =
       Math.abs(this.clientX - e.clientX) > DRAG_THRESHOLD ||
       Math.abs(this.clientY - e.clientY) > DRAG_THRESHOLD;
@@ -103,11 +124,8 @@ export default class WordCard extends Component {
   }
 
   private dropHandler(e: MouseEvent) {
-    // find the target cell underneath the dragged card
-    const elementsBelow = document.elementsFromPoint(e.clientX, e.clientY);
-    const dropTarget = elementsBelow.find((element) =>
-      element.classList.contains(rowStyles.cell),
-    );
+    // cannot use this.lastDropTarget here, because drop canceling wouldn't work
+    const dropTarget = WordCard.findDroppableCell(e.clientX, e.clientY);
 
     if (!dropTarget) {
       // basically, nothing happens, but we need to synchronize the UI with the state because we snapped the card from the row, leaving a gap
@@ -124,8 +142,7 @@ export default class WordCard extends Component {
       this.actionHandler(action);
     }
 
-    // TODO: create assertion function
-    if (dropTarget instanceof HTMLElement) {
+    if (dropTarget) {
       const action: WordAction = {
         type: "drop/swap",
         payload: {
@@ -145,5 +162,31 @@ export default class WordCard extends Component {
 
     card.style.left = `${pageX - this.shiftX}px`;
     card.style.top = `${pageY - this.shiftY}px`;
+  }
+
+  private setDropTargetHoverStyles() {
+    if (this.lastDropTarget) {
+      this.lastDropTarget.style.background = "var(--color-brand-200)";
+    }
+  }
+
+  private resetDropTargetHoverStyles() {
+    if (this.lastDropTarget) {
+      this.lastDropTarget.style.background = "";
+    }
+  }
+
+  // NOTE: I don't know about this, but eslint insists
+  private static findDroppableCell(
+    clientX: number,
+    clientY: number,
+  ): HTMLElement | null {
+    // find the target cell underneath the dragged card
+    const elementsBelow = document.elementsFromPoint(clientX, clientY);
+    const dropTarget = elementsBelow.find((element) =>
+      element.classList.contains(rowStyles.cell),
+    );
+
+    return dropTarget instanceof HTMLElement ? dropTarget : null;
   }
 }
