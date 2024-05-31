0b9b83a4ebced16f1a1f451102c0c14d5aa0370c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-31 13:57:12 +0300

    feat: handle word actions

--- src/features/game/model/GameState.ts
+++ src/features/game/model/GameState.ts
@@ -1,32 +1,10 @@
 import State from "../../../app/state/StatePublisher";
-import { Word } from "../card/WordCard";
+import { GameData, StageStatus, Word, WordAction } from "../types";
 
-import { getShuffledSentence, splitSentence } from "../../../shared/helpers";
 import rawData from "../../../../data/words.json";
+import { getShuffledSentence, splitSentence } from "../../../shared/helpers";
 
 const STAGES_PER_ROUND = 10;
-
-export enum StageStatus {
-  NOT_COMPLETED,
-  CORRECT,
-  INCORRECT,
-  AUTOCOMPLETED,
-}
-
-interface GameData {
-  levels: {
-    round: number;
-    stage: number;
-    status: StageStatus;
-  };
-  content: {
-    roundSentences: Array<string>;
-    sentence: string;
-    sentenceLength: number;
-    pickArea: Array<Word | null>;
-    assembleArea: Array<Word | null>;
-  };
-}
 
 function prepareState(round: number, stage: number): GameData {
   const roundSentences = rawData.rounds[round].words.map(
@@ -58,6 +36,39 @@ export default class GameState extends State<GameData> {
     super(prepareState(0, 0));
   }
 
+  actionHandler(action: WordAction) {
+    const { rowTo, rowFrom, indexTo, indexFrom } = action.payload;
+    const areaTo = this.state.content[rowTo];
+    const areaFrom = this.state.content[rowFrom];
+
+    switch (action.type) {
+      case "drop/swap": {
+        // swap
+        [areaFrom[indexFrom], areaTo[indexTo]] = [
+          areaTo[indexTo],
+          areaFrom[indexFrom],
+        ];
+
+        this.notifySubscribers();
+        break;
+      }
+      case "drop/cancel":
+        this.notifySubscribers();
+        break;
+      case "click/move": {
+        // insert in the first empty cell
+        const insertPosition = areaTo.indexOf(null);
+        areaTo[insertPosition] = areaFrom[indexFrom];
+        areaFrom[indexFrom] = null;
+
+        this.notifySubscribers();
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
   startGame() {
     this.notifySubscribers();
   }
@@ -69,42 +80,6 @@ export default class GameState extends State<GameData> {
     if (this.state.levels.stage === STAGES_PER_ROUND - 1) return;
 
     this.state = prepareState(0, this.state.levels.stage + 1);
-
-    this.notifySubscribers();
-  }
-
-  pickWord(word: Word | null) {
-    const { pickArea, assembleArea } = this.state.content;
-
-    const target = word;
-    if (!target) return;
-
-    // put in first empty cell
-    const positionToPut = assembleArea.indexOf(null);
-    assembleArea[positionToPut] = word;
-
-    // replace with null, then update position property
-    pickArea[target.currentPosition] = null;
-    target.currentPosition = positionToPut;
-
-    this.notifySubscribers();
-  }
-
-  discardWord(word: Word | null) {
-    const { pickArea, assembleArea } = this.state.content;
-    const target = word;
-
-    if (!target) return;
-
-    // put in first empty cell
-    const positionToPut = pickArea.indexOf(null);
-    pickArea[positionToPut] = word;
-
-    // replace with null, then update position property
-    assembleArea[target.currentPosition] = null;
-    target.currentPosition = positionToPut;
-
-    this.state.levels.status = StageStatus.NOT_COMPLETED;
 
     this.notifySubscribers();
   }
@@ -134,27 +109,5 @@ export default class GameState extends State<GameData> {
 
   isFilled() {
     return this.state.content.assembleArea.every((item) => item);
-  }
-
-  dropWord(from: number, to: number | null) {
-    if (to === null) {
-      this.notifySubscribers();
-      return;
-    }
-
-    const newWord = this.state.content.pickArea[from];
-    const oldWord = this.state.content.assembleArea[to];
-
-    if (newWord) {
-      newWord.currentPosition = to;
-    }
-    if (oldWord) {
-      oldWord.currentPosition = from;
-    }
-
-    this.state.content.assembleArea[to] = newWord;
-    this.state.content.pickArea[from] = oldWord;
-
-    this.notifySubscribers();
   }
 }
