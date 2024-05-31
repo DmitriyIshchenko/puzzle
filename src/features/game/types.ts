236576a5e54ac9f18e22ed17cdfbc9f5ddbd0f66
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-05-31 13:54:42 +0300

    fix: avoid dependency cycles by moving types into separate file

---
+++ src/features/game/types.ts
@@ -0,0 +1,40 @@
+export interface Word {
+  text: string;
+  width: number;
+  readonly correctPosition: number;
+  currentPosition: number;
+}
+
+export interface WordAction {
+  type: string;
+  payload: {
+    indexFrom: number;
+    rowFrom: RowType;
+    indexTo: number;
+    rowTo: RowType;
+  };
+}
+export enum RowType {
+  PICK = "pickArea",
+  ASSEMBLE = "assembleArea",
+}
+export enum StageStatus {
+  NOT_COMPLETED,
+  CORRECT,
+  INCORRECT,
+  AUTOCOMPLETED,
+}
+export interface GameData {
+  levels: {
+    round: number;
+    stage: number;
+    status: StageStatus;
+  };
+  content: {
+    roundSentences: Array<string>;
+    sentence: string;
+    sentenceLength: number;
+    pickArea: Array<Word | null>;
+    assembleArea: Array<Word | null>;
+  };
+}
