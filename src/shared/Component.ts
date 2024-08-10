327f7839a256b77d732249f948619347334f05a9
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-08-10 14:39:03 +0300

    feat: add resetClasses method

--- src/shared/Component.ts
+++ src/shared/Component.ts
@@ -104,4 +104,12 @@ export default class Component<T extends HTMLElement = HTMLElement> {
   removeClass(className: string) {
     this.element.classList.remove(className);
   }
+
+  resetClasses(newClassName?: string) {
+    this.element.className = "";
+
+    if (newClassName) {
+      this.addClass(newClassName);
+    }
+  }
 }
