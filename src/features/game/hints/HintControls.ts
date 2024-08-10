37997bf6d51c7f14d76e87054a03d8acd4fd722e
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-08-10 15:36:02 +0300

    fix: create a setting key type guard

--- src/features/game/hints/HintControls.ts
+++ src/features/game/hints/HintControls.ts
@@ -7,6 +7,7 @@ import { i, input, label } from "../../../ui/tags";
 
 import styles from "./HintControls.module.css";
 
+// Record maps each key to an object
 const iconClasses: Record<keyof HintSettingsData, { on: string; off: string }> =
   {
     translation: {
@@ -18,11 +19,6 @@ const iconClasses: Record<keyof HintSettingsData, { on: string; off: string }> =
       off: "bi bi-volume-mute",
     },
   };
-
-// type guard
-function isKeyOfHintSettingsData(key: string): key is keyof HintSettingsData {
-  return key in ({} as HintSettingsData);
-}
 
 export default class HintsControls extends Component implements Observer {
   constructor(private hintSettings: HintSettings) {
@@ -36,26 +32,28 @@ export default class HintsControls extends Component implements Observer {
     this.appendChildren([
       label(
         { className: styles.button },
-        i({ className: iconClasses.translation.on }),
-        input({ type: "checkbox", name: "translation" }),
+        i({ className: iconClasses.audio.on }),
+        input({ type: "checkbox", id: "audio" }),
       ),
       label(
         { className: styles.button },
-        i({ className: iconClasses.audio.on }),
-        input({ type: "checkbox", name: "audio" }),
+        i({ className: iconClasses.translation.on }),
+        input({ type: "checkbox", id: "translation" }),
       ),
     ]);
 
     this.addListener("change", this.handleChangeSetting.bind(this));
   }
 
+  // NOTE: this is a type guard defined as a class method (this way I can get access to the state object for checking). I don't really know whether it is a good idea or a bad one.
+  private isValidSetting(key: string): key is keyof HintSettingsData {
+    return key in this.hintSettings.state;
+  }
+
   private handleChangeSetting(e: Event) {
     const { target } = e;
-    if (
-      target instanceof HTMLInputElement &&
-      isKeyOfHintSettingsData(target.name)
-    ) {
-      this.hintSettings.toggleSetting(target.name);
+    if (target instanceof HTMLInputElement && this.isValidSetting(target.id)) {
+      this.hintSettings.toggleSetting(target.id);
     }
   }
 
@@ -67,16 +65,16 @@ export default class HintsControls extends Component implements Observer {
 
         if (
           checkboxEl instanceof HTMLInputElement &&
-          isKeyOfHintSettingsData(checkboxEl.name)
+          this.isValidSetting(checkboxEl.id)
         ) {
           // update value
-          checkboxEl.checked = publisher.state[checkboxEl.name];
+          checkboxEl.checked = publisher.state[checkboxEl.id];
 
           // update icon
           icon.resetClasses(
             checkboxEl.checked
-              ? iconClasses[checkboxEl.name].on
-              : iconClasses[checkboxEl.name].off,
+              ? iconClasses[checkboxEl.id].on
+              : iconClasses[checkboxEl.id].off,
           );
         }
       });
