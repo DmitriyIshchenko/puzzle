11bcaebf15fa14f0f86017cefb7b4e4af13c2c8f
Dmitriy Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-08-13 18:33:25 +0300

    feat: PZ-24 Implement a hint state persistence (#74)
    * feat: load hint settings from local storage
    
    * feat: save hint settings to local storage on every update
    
    * feat: reset settings on logout

--- src/app/state/StatePublisher.ts
+++ src/app/state/StatePublisher.ts
@@ -3,11 +3,28 @@ import { Publisher, Observer } from "../../shared/Observer";
 export default class State<T> implements Publisher {
   public state: T;
 
-  constructor(initialState: T) {
-    this.state = initialState;
+  constructor(
+    private defaultState: T,
+    private key: string = "",
+  ) {
+    this.state = this.loadState();
   }
 
   private subscribers: Array<Observer> = [];
+
+  private loadState() {
+    const stateString = localStorage.getItem(this.key);
+
+    if (stateString) return JSON.parse(stateString) as T;
+
+    return this.defaultState;
+  }
+
+  saveState() {
+    if (this.key) {
+      localStorage.setItem(this.key, JSON.stringify(this.state));
+    }
+  }
 
   subscribe(subscriber: Observer): void {
     this.subscribers.push(subscriber);
