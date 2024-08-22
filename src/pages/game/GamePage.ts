5b3134472397a409ba2ae44197d3d468088fa1e6
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-08-22 17:49:17 +0300

    refactor: split the configure() function into multiple smaller functions

--- src/pages/game/GamePage.ts
+++ src/pages/game/GamePage.ts
@@ -8,10 +8,11 @@ import GameControls from "../../features/game/controls/GameControls";
 import TranslationHint from "../../features/game/hints/TranslationHint";
 import PronunciationHint from "../../features/game/hints/PronunciationHint";
 import HintsControls from "../../features/game/hints/HintControls";
-import { div } from "../../ui/tags";
 import HintSettings from "../../features/game/model/HintSettings";
 import RoundControls from "../../features/game/controls/RoundControls";
 import RoundSettings from "../../features/game/model/RoundSettings";
+
+import { div } from "../../ui/tags";
 
 export default class GamePage extends Component {
   roundSettings: RoundSettings;
@@ -34,10 +35,25 @@ export default class GamePage extends Component {
   }
 
   private configure() {
+    const controls = this.configureControls();
+    const hints = this.configurHints();
+    const fields = this.configureFieds();
+
+    this.appendChildren([controls, hints, ...fields]);
+
+    this.roundState.startRound();
+    this.hintSettings.notifySubscribers();
+  }
+
+  private configureFieds() {
     const gameField = new GameField(this.roundState, this.hintSettings);
     const wordsPicker = new WordsPicker(this.roundState, this.hintSettings);
-    const stageControls = new GameControls(this.roundState);
-    const hintControls = new HintsControls(this.hintSettings);
+    const roundControls = new GameControls(this.roundState);
+
+    return [gameField, wordsPicker, roundControls];
+  }
+
+  private configurHints() {
     const translationHint = new TranslationHint(
       this.roundState,
       this.hintSettings,
@@ -46,6 +62,19 @@ export default class GamePage extends Component {
       this.roundState,
       this.hintSettings,
     );
+
+    const hints = div(
+      { className: styles.hints },
+      pronunciationHint,
+      translationHint,
+    );
+
+    return hints;
+  }
+
+  private configureControls() {
+    const hintControls = new HintsControls(this.hintSettings);
+
     const roundControls = new RoundControls(this.roundSettings);
 
     const controls = div(
@@ -56,21 +85,6 @@ export default class GamePage extends Component {
       hintControls,
     );
 
-    const hints = div(
-      { className: styles.hints },
-      pronunciationHint,
-      translationHint,
-    );
-
-    this.appendChildren([
-      controls,
-      hints,
-      gameField,
-      wordsPicker,
-      stageControls,
-    ]);
-
-    this.roundState.startRound();
-    this.hintSettings.notifySubscribers();
+    return controls;
   }
 }
